50698abe9f72c70fd528e88f6dca921490a98bdf
Dennis Wei <dw2654@columbia.edu>
2021-12-08 22:06:35 -0800

    day 9 (232, 522)

---
+++ aoc_elixir/solutions/9.exs
@@ -0,0 +1,91 @@
+Code.require_file("lib/input.ex")
+filename = "input/9.txt"
+# filename = "test_input/9.txt"
+input = Input
+  # .ints(filename)
+  # .line_tokens(filename)
+  .lines(filename)
+  # .line_of_ints(filename)
+
+defmodule Day9 do
+  def to_map(glist) do
+    Map.new(glist, fn ent ->
+      {n, r, c} = ent
+      {{r, c}, n}
+    end)
+  end
+
+  def make_grid(rows) do
+    as_list = rows
+      |> Enum.map(&String.trim/1)
+      |> Enum.map(&String.graphemes/1)
+      |> Enum.map(fn r -> Enum.map(r, &String.to_integer/1) end)
+      |> Enum.map(&Enum.with_index/1)
+      |> then(&Enum.with_index/1)
+      |> Enum.flat_map(fn {row, ridx} -> Enum.map(row, fn e -> Tuple.insert_at(e, 1, ridx) end) end)
+
+    to_map(as_list)
+  end
+
+  @neighbors [{0, 1}, {0, -1}, {1, 0}, {-1, 0}]
+  def neighbors, do: @neighbors
+
+  def get_neighbors(grid, {x, y}) do
+    Enum.reduce(Day9.neighbors(), [], fn ({dx, dy}, acc) ->
+      adj = {x + dx, y + dy}
+      case Map.get(grid, adj) do
+        nil -> acc
+        n -> [{adj, n} | acc]
+      end
+    end)
+  end
+
+  def recurse_inner(grid, {{x, y}, n}, {traversed, acc}) do
+    traversed = MapSet.put(traversed, {x, y})
+    acc = [{{x, y}, n} | acc]
+
+    Day9.get_neighbors(grid, {x, y})
+      |> Enum.reduce({traversed, acc}, fn ({adj, nadj}, {traversed, acc}) ->
+        case nadj > n && nadj != 9 do
+          true -> Day9.recurse(grid, {adj, nadj}, {traversed, acc})
+          false -> {traversed, acc}
+        end
+      end)
+  end
+
+  def recurse(grid, {{x, y}, n}, {traversed, acc}) do
+    case MapSet.member?(traversed, {x, y}) do
+      true -> {traversed, acc}
+      false -> Day9.recurse_inner(grid, {{x, y}, n}, {traversed, acc})
+    end
+
+  end
+end
+
+grid = Day9.make_grid(input)
+
+{part1, lps} = grid
+  |> Enum.reduce({0, []}, fn ({{x, y}, n}, {p1_acc, lps}) ->
+    is_lp = Day9.get_neighbors(grid, {x, y})
+      |> Enum.all?(fn {_adj, nadj} -> nadj > n end)
+
+    case is_lp do
+      true -> {p1_acc + n + 1, [{{x, y}, n} | lps]}
+      false -> {p1_acc, lps}
+    end
+  end)
+
+basins = lps
+  |> Map.new(fn {{x, y}, n} -> {{{x, y}, n}, Day9.recurse(grid, {{x, y}, n}, {MapSet.new(), []})} end)
+
+basin_sizes = basins
+  |> Map.new(fn {k, {_traversed, acc}} -> {k, length(acc)} end)
+
+part2 = basin_sizes
+  |> Map.values
+  |> Enum.sort(:desc)
+  |> Enum.take(3)
+  |> Enum.product
+
+IO.puts("Part 1: #{part1}")
+IO.puts("Part 2: #{part2}")
---
+++ python/9/run.py
@@ -0,0 +1,96 @@
+import time
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+import argparse
+import clipboard
+
+from itertools import combinations
+from collections import defaultdict, Counter, deque
+from util.helpers import split_newline, space_split, int_parsed_list, list_of_ints, get_all_nums, submit, Input
+from math import floor, ceil
+from functools import reduce
+from copy import deepcopy
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--from-std-in", action='store_true', default=False)
+args = parser.parse_args()
+if args.from_std_in:
+    raw_input = clipboard.paste()
+else:
+    try:
+        with open("input.txt", "r") as f:
+            raw_input = f.read()
+    except:
+        try:
+            with open("9/input.txt", "r") as f:
+                raw_input = f.read()
+        except:
+            with open("python/9/input.txt", "r") as f:
+                raw_input  = f.read()
+raw_input = raw_input.strip()
+
+input = (
+    Input(raw_input)
+        # .all()
+        # .ints()
+        # .int_tokens()
+        # .tokens()
+        .lines()
+        # .line_tokens()
+        # .line_tokens(sep = "\n", line_sep = "\n\n")
+)
+
+def recurse(grid, point, n, traversed, acc):
+    (x, y) = point
+    traversed.add(point)
+    acc.append(point)
+    for (dx, dy) in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        padj = (x + dx, y + dy)
+        if padj in grid and grid[padj] > n and grid[padj] != 9:
+            recurse(grid, padj, grid[padj], traversed, acc)
+
+def solve(input):
+    grid = {}
+    for i, row in enumerate(input):
+        for j, c in enumerate(row):
+            grid[(i, j)] = int(c)
+    
+    mins = 0
+    min_points = []
+    for (x, y), n in grid.items():
+        is_min = True
+        for (dx, dy) in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            padj = (x + dx, y + dy)
+            if padj in grid and grid[padj] <= n:
+                is_min = False
+        if is_min:
+            min_points.append((x, y))
+            mins += n + 1
+    
+    basins = {}
+    basin_sizes = {}
+    for (x, y) in min_points:
+        acc = []
+        traversed = set()
+        recurse(grid, (x, y), grid[(x, y)], traversed, acc)
+
+        deduped = set(acc)
+        basin_sizes[(x, y)] = len(deduped)
+        basins[(x, y)] = deduped
+    
+    top = sorted(basin_sizes.values(), reverse = True)[:3]
+    p2 = top[0] * top[1] * top[2]
+
+    return mins, p2
+
+start = time.time()
+answer1, answer2 = solve(input)
+
+print("Part 1")
+print(f"Answer: {answer1}")
+# print(submit(9, 1, answer1).text)
+
+print("Part 2")
+print(f"Answer: {answer2}")
+# print(submit(9, 2, answer1).text)
+print(f"Took {time.time() - start} seconds for both parts")
